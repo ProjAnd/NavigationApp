87bf28f843572142fe5aacdd0a23b54f3a0da66c
Dell <hr.mobix@gmail.com>
2023-08-11 22:54:37 +0530

    Initial Code

---
+++ app/src/main/java/com/example/appnavigatio/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.appnavigatio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.NavHost
+
+class MainActivity : AppCompatActivity() {
+
+    override fun setContentView(layoutResID: Int) {
+        super.setContentView(R.layout.activity_main)
+    }
+
+
+}
